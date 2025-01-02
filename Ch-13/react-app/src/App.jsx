55f6dfb14971414d3280c27c6ffb823116ab17c6
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2025-01-02 15:57:35 +0530

    useRef-Hook

--- Ch-13/react-app/src/App.jsx
+++ Ch-13/react-app/src/App.jsx
@@ -1,60 +1,118 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 
-import './App.css'
-import { useEffect } from 'react'
-import { useRef } from 'react'
+// import './App.css'
+// import { useEffect } from 'react'
+// import { useRef } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-  // let val=0
+// function App() {
+//   const [count, setCount] = useState(0)
+//   // let val=0
   
-let vall=useRef(0)
- let btnRef=useRef()
+// let vall=useRef(0)
+//  let btnRef=useRef()
 
  
- function handleIncrement(){
-   // val=val+1
-   // console.log("the value of val",val)
-   vall.current=vall.current+1
-   console.log("the value of vall",vall.current)
-   setCount(count+1)
-  }
+//  function handleIncrement(){
+//    // val=val+1
+//    // console.log("the value of val",val)
+//    vall.current=vall.current+1
+//    console.log("the value of vall",vall.current)
+//    setCount(count+1)
+//   }
   
-  useEffect(()=>{
-    console.log("main firse re-render hogy hu")
-  })
+//   useEffect(()=>{
+//     console.log("main firse re-render hogy hu")
+//   })
 
 
-  function changeColor(){
-  btnRef.current.style.backgroundColor="red"
-  }
+//   function changeColor(){
+//   btnRef.current.style.backgroundColor="red"
+//   }
 
-  return (
+//   return (
 
-    <div>
+//     <div>
   
-  count:{count}
+//   count:{count}
 
-  <br />
-  <br />
+//   <br />
+//   <br />
  
  
-     <button ref={btnRef} onClick={handleIncrement}>
-     Increment
-     </button>
+//      <button ref={btnRef} onClick={handleIncrement}>
+//      Increment
+//      </button>
    
+//       <br />
+//       <br />
+//       <br />
+
+//       <button onClick={changeColor}>
+//     change increment button color
+//      </button>     
+
+
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+
+import React from "react";
+import './app.css'
+import { useState } from "react";
+import { useRef } from "react";
+
+function App(){
+
+  const [time,setTime]=useState(0)
+
+  let timerRef=useRef(null)
+
+   function startTimer(){
+   timerRef.current= setInterval(() => {
+      setTime((time)=>time+1)
+    }, 1000);
+   }
+
+   function stopTimer(){
+ clearInterval(timerRef.current)
+ 
+
+   }
+
+   function resetTimer(){
+  stopTimer()
+  setTime(0)
+   }
+
+  return(
+    <div>
+      StopWatch: {time} seconds
       <br />
       <br />
+      <button onClick={startTimer}>
+        Start
+      </button>
       <br />
-
-      <button onClick={changeColor}>
-    change increment button color
-     </button>     
-
-
-
+      <br />
+      <button onClick={stopTimer}>
+        Stop
+      </button>
+      <br />
+      <br />
+      <button onClick={resetTimer}>
+        Reset
+      </button>
     </div>
   )
+
+
+
 }
 
-export default App
+export default App;
