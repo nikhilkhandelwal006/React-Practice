1130f330878acf92e61333cb13ab3df105b42a65
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2025-01-04 18:14:55 +0530

    use-Callback hook

--- Ch-15/react-app/src/components/ChildComponent.jsx
+++ Ch-15/react-app/src/components/ChildComponent.jsx
@@ -13,4 +13,4 @@ export default ChildComponent
 //React.memo k andar wrap krdete h components ko toh component tbhi re-render hoga jb props change honge, nii toh re-render ni hoga.
 
 
-//if u r sending a function, then react.memo won't be able to save you from re-rendering
+//if u r sending a function, then react.memo won't be able to save you from re-rendering.But why? because re-render hone se function ka by-default refrence bhi change hojata h jis wajah se child-component ko lga ki ye different function h uss wajah se child component wapis re-render hogy
